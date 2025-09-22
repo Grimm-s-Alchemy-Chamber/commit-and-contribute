5042331c3c09e8bea3665d37a951b8ff5f3e082d
holygrimm <abhaygp18.dev@gmail.com>
2025-09-22 12:20:15 +0530

    added ui components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import VisualEditsMessenger from "../visual-edits/VisualEditsMessenger";
 import ErrorReporter from "@/components/ErrorReporter";
 import Script from "next/script";
 import Header from "@/components/site/Header";
@@ -31,7 +30,6 @@ export default function RootLayout({
         />
         <Header />
         <main>{children}</main>
-        <VisualEditsMessenger />
       </body>
     </html>
   );

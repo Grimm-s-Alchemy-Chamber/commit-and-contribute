28e87b2596f7c59c185445168e948578d6bdcd1b
John Aerial J. Azcune <65322242+STICKnoLOGIC@users.noreply.github.com>
2025-10-20 11:00:46 +0800

    Update slugify function to include uppercase letters

--- src/lib/slugify.ts
+++ src/lib/slugify.ts
@@ -1,3 +1,3 @@
 export function slugify(input: string): string {
-    return input.replace(/[^a-z0-9]+/g, "-").replace(/(^-|-$)/g, "");
+    return input.replace(/[^a-zA-Z0-9]+/g, "-").replace(/(^-|-$)/g, "");
 }
